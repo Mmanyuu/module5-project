5c436166ccdec0be0557a59c5a574fac41d2897f
cloudi32003 <cloudi32003@gmail.com>
2025-02-18 20:31:36 +0800

    Calendar v2

--- project-mobile-app/App.js
+++ project-mobile-app/App.js
@@ -1,22 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CompassScreen from './src/screens/CompassScreen';
+// Importing the Modules
+import React, { useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { CalendarList } from "react-native-calendars";
+// import Day from "react-native-calendars/src/calendar/day/basic";
 
-export default function App() {
+const App = () => {
+  const [selected, setSelected] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <CompassScreen></CompassScreen>
+      <CalendarList
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={3}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={3}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedDotColor: "orange",
+          },
+        }}
+      />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
