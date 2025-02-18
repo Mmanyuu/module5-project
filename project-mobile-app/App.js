56f59c56ab8b8a35f7ae3c5979dfa70caf67c559
cloudi32003 <cloudi32003@gmail.com>
2025-02-18 21:36:13 +0800

    Calendar v3

--- project-mobile-app/App.js
+++ project-mobile-app/App.js
@@ -1,45 +1,31 @@
 // Importing the Modules
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import { CalendarList } from "react-native-calendars";
 // import Day from "react-native-calendars/src/calendar/day/basic";
 
-const App = () => {
-  const [selected, setSelected] = useState("");
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
+import HomeScreen from "./screen/HomeScreen";
+// import AgendaScreen from "./screen/AgendaScreen";
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <CalendarList
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={3}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={3}
-        // Enable or disable scrolling of calendar list
-        scrollEnabled={true}
-        // Enable or disable vertical scroll indicator. Default = false
-        showScrollIndicator={true}
-        onDayPress={(day) => {
-          setSelected(day.dateString);
-        }}
-        markedDates={{
-          [selected]: {
-            selected: true,
-            disableTouchEvent: true,
-            selectedDotColor: "orange",
-          },
-        }}
-      />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
   );
-};
+}
 
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
